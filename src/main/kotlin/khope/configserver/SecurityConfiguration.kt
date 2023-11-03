416a930f77083327012168350679fca047121c33
aesperer <piyrw9754@gmail.com>
2023-11-03 15:16:22 +0900

    feature :: @Bean 어노테이션 configure 메서드에 추가했습니다.

--- src/main/kotlin/khope/configserver/SecurityConfiguration.kt
+++ src/main/kotlin/khope/configserver/SecurityConfiguration.kt
@@ -1,11 +1,13 @@
 package khope.configserver
 
+import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @EnableWebSecurity
 class SecurityConfiguration {
 
+    @Bean
     protected fun configure(http: HttpSecurity) {
         http
             .formLogin()
